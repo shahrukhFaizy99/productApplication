756d4796bef2c6774dadda51306958d02666b2d0
shahrukhFaizy99 <shahrukhfaizy17@gmail.com>
2021-11-30 19:46:26 +0500

    third-Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Authentication from "./Pages/Authentication/Authentication";
+import Category from "./Pages/Category/Category";
+import CategoryProducts from "./Pages/CategoryProduct/CategoryProducts";
+import Checkout from "./Pages/Checkout/Checkout";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/authentication" component={Authentication} />
+      <Route path="/category" component={Category} />
+      <Route path="/categoryProducts" component={CategoryProducts} />
+      <Route path="/checkout" component={Checkout} />
+    </Switch>
   );
 }
 
